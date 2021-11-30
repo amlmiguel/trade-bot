2678786e0c2077c9431357ec2135b396123ac291
Miguel Leite <amlmiguel@gmail.com>
2021-11-30 09:40:09 -0300

    Consertando bugs

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ setInterval(async () => {
     const result = await api.depth(symbol);
     if(result.bids && result.bids.length){
         console.log(`Maior preço de compra: ${result.bids[0][0]}`);
-        buy = parseInt(result.bids[0][0]);
+        buy = parseFloat(result.bids[0][0]);
     }
 
     if(result.asks && result.asks.length){
         console.log(`Maior preço de venda: ${result.asks[0][0]}`);
-        sell = parseInt(result.asks[0][0])
+        sell = parseFloat(result.asks[0][0])
     }
 
 
-    if(sell < 56000) {
+    if(sell && sell < 58000) {
         console.log('Hora de comprar');
 
         const account = await api.accountInfo();
@@ -30,24 +30,27 @@ setInterval(async () => {
         const valorQtd = sell * qtdInputada;
 
         console.log('Verificando se tenho grana...');
+        const carteiraUSD = parseFloat(coins.find(c => c.asset === 'USDT').free);
             
-        if(valorQtd <= parseInt(coins.find(c => c.asset === 'USDT').free)){
+        if(valorQtd <= carteiraUSD){
             console.log("Temos grana, vamos comprar (To the moon) !!!");
             const buyOrder = await api.newOrder(symbol, qtdInputada);
             console.log(`orderId: ${buyOrder.orderId}`);
             console.log(`status: ${buyOrder.status}`);
            
+            if(buyOrder === 'FILLED'){
+                console.log('Posicionando venda futura...');
+                const price = parseFloat(valorQtd * profitability).toFixed(2);
+                console.log(`Vendendo por ${price} (${profitability})`);
+                const sellOrder = await api.newOrder(symbol, qtdInputada, price, 'SELL', 'LIMIT');
+                console.log(`orderId: ${sellOrder.orderId}`);
+                console.log(`status: ${sellOrder.status}`);  
 
-            console.log('Posicionando venda futura...');
-            const price = parseFloat(valorQtd * profitability);
-            console.log(`Vendendo por ${price} (${profitability})`);
-            const sellOrder = await api.newOrder(symbol, qtdInputada, price.toFixed(2), 'SELL', 'LIMIT');
-            console.log(`orderId: ${sellOrder.orderId}`);
-            console.log(`status: ${sellOrder.status}`);      
-        }
-        
+            }
+                
+        }  
 
-    }else if(buy > 62000) {
+    }else if(buy && buy > 62000) {
         console.log('Hora de vender');
     }else{
         console.log('Fica quieto !');
